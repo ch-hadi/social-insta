b3b6d1897993c180eb273d1f02a4fa266d2741b0
ch-hadi <mrhammad.009@gmail.com>
2023-08-03 00:42:58 +0500

    model completed

--- src/components/CModal/CModal.jsx
+++ src/components/CModal/CModal.jsx
@@ -1,23 +1,16 @@
 import * as React from 'react';
 
 import Modal from '@mui/material/Modal';
-
-const style = {
-  display: 'flex',
-  flexDirection: 'column',
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 600,
-
-  //   border: '2px solid #000',
-  //   boxShadow: 24,
-  //   borderRadius: '10px',
-};
+import { Box, TextField, Button } from '@mui/material';
 
 export default function CModal(props) {
+  const [expand, setExpand] = React.useState(false);
   const [image, setImage] = React.useState(null);
+  const [descreption, setDescription] = React.useState('');
+
+  const handleChange = (e) => {
+    setDescription(e.target.value);
+  };
 
   const handleImageChange = (e) => {
     const file = e.target.files[0];
@@ -30,46 +23,83 @@ export default function CModal(props) {
     }
   };
 
+  const createPost = () => {
+    const formData = new FormData();
+    formData.append = ('img', image);
+    console.log('created !');
+  };
+
   return (
-    <div>
-      <Modal open={props.open} onClose={props.handleClose}>
-        <div style={style} className='bg-white h-[400px]'>
-          <div className=' bg-white flex justify-center'>
-            <p>Create Post</p>
-          </div>
-          <div className='relative w-full h-full'>
-            <div
-              style={{
-                position: image ? 'relative' : 'absolute',
-                top: image ? '' : '50%',
-                left: image ? '' : '50%',
-                transform: image ? '' : 'translate(-50%, -50%)',
-              }}>
+    <Modal open={props.open} onClose={props.handleClose}>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          width: expand ? 900 : 600,
+          borderRadius: 10,
+        }}
+        className={`bg-white ${!image ? 'h-[440px]' : ''} p-2 overflow-hidden`}>
+        <div className=' bg-white-400 shadow-sm py-2 rounded text-black-400 flex justify-center '>
+          <p>Create Post</p>
+          {image ? (
+            <span
+              onClick={() => setExpand(true)}
+              className='absolute cursor-pointer right-10 text-blue-600'>
+              Next
+            </span>
+          ) : (
+            ''
+          )}
+        </div>
+
+        <div className='relative w-full h-full'>
+          <div
+            style={{
+              position: image ? 'relative' : 'absolute',
+              top: image ? '' : '50%',
+              left: image ? '' : '50%',
+              transform: image ? '' : 'translate(-50%, -50%)',
+            }}>
+            {image ? (
+              ''
+            ) : (
               <label
                 className='bg-blue-800 text-white p-2 rounded'
                 htmlFor='upload-button'>
                 <span>Choose Image</span>
               </label>
-              <input
-                type='file'
-                id='upload-button'
-                accept='image/*'
-                onChange={handleImageChange}
-                style={{ display: 'none' }}
-              />
-              {image && (
-                <div className='w-full flex justify-center h-[350px]  overflow-hidden'>
-                  <img
-                    src={image}
-                    alt='Preview'
-                    style={{ width: '100%', objectFit: 'contain' }}
-                  />
-                </div>
-              )}
-            </div>
+            )}
+            <input
+              type='file'
+              id='upload-button'
+              accept='image/*'
+              onChange={handleImageChange}
+              style={{ display: 'none' }}
+            />
+            {image && (
+              <div className='w-full flex justify-center h-[350px]  overflow-hidden'>
+                <img
+                  src={image}
+                  alt='Preview'
+                  style={{
+                    width: '100%',
+                    borderRadius: 10,
+                    objectFit: 'contain',
+                    marginTop: 5,
+                  }}
+                />
+              </div>
+            )}
           </div>
         </div>
-      </Modal>
-    </div>
+        <Button onClick={createPost} variant='outlined' sx={{ marginTop: 2 }}>
+          Create
+        </Button>
+      </div>
+    </Modal>
   );
 }
